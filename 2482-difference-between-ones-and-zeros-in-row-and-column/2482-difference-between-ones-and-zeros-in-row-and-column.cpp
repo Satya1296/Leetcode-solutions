7e6f3e5fe9a7c87d32361b70e24cb05b9b872b9a
Satya1296 <asrithasatya12@gmail.com>
2025-12-19 06:36:42 +0530

    Time: 26 ms (90.64%), Space: 121.9 MB (44.04%) - LeetHub

---
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<int>onerow(m,0);
+        vector<int>onecol(n,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    onerow[i]++;
+                    onecol[j]++;
+                }
+            }
+        }
+        vector<vector<int>>res(m,vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                res[i][j]=onerow[i]+onecol[j]-(m-onerow[i])-(n-onecol[j]);
+            }
+        }
+        return res;
+    }
+};
