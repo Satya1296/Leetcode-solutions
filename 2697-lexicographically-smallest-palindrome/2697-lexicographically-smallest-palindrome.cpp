511c108fb4fbadfc0dd782a6c80f43c8ddba8f97
Satya1296 <asrithasatya12@gmail.com>
2026-01-03 06:17:12 +0530

    Time: 5 ms (51.87%), Space: 18.6 MB (59.55%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                if(s[i]<s[j]){
+                    s[j]=s[i];
+                }
+                s[i]=s[j];
+            }
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
