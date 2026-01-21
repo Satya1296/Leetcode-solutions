3c5cf2a6514715c60177ff772ebadeda2451dc58
Satya1296 <asrithasatya12@gmail.com>
2026-01-21 06:28:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int time=arrivalTime+delayedTime;
+        if(time==24) return 0;
+        else return time;
+    }
+};
