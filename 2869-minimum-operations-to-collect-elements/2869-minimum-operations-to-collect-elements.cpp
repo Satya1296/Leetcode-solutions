eaaef0f1526551d0620d5b3e1d450692f49d0b57
Satya1296 <asrithasatya12@gmail.com>
2026-01-31 06:27:04 +0530

    Time: 0 ms (100%), Space: 23 MB (24.38%) - LeetHub

---
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_set<int>s;
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<=k){
+                s.insert(nums[i]); // 2 1 
+            }
+            if(s.size()==k){
+                return n-i;
+            }
+        }
+        return n;
+    }
+};
