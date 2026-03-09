c9fe1816c9adc0e29adf76617620208f40dbe3aa
Satya1296 <asrithasatya12@gmail.com>
2026-03-09 06:36:56 +0530

    Time: 0 ms (100%), Space: 9.2 MB (29.18%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls=0,cows=0;
+        unordered_map<char,int>mpp;
+        for(int i=0;i<secret.size();i++){
+            if(secret[i]==guess[i]){
+                bulls++;
+            }
+            else{
+                mpp[secret[i]]++;
+            }
+        }
+        for(int i=0;i<guess.size();i++){
+            if(secret[i]!=guess[i] && mpp[guess[i]]>0){
+                cows++;
+                mpp[guess[i]]--;
+            }
+        }
+        return to_string(bulls)+"A"+to_string(cows)+"B";
+    }
+};
