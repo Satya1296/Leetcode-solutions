733ffed40d48e66b308d49393fb2fefa49ffaf6c
Satya1296 <asrithasatya12@gmail.com>
2026-04-17 11:43:06 +0530

    Time: 0 ms (100%), Space: 26.7 MB (31.72%) - LeetHub

---
+++ 3502-minimum-cost-to-reach-every-position/3502-minimum-cost-to-reach-every-position.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> minCosts(vector<int>& cost) {
+        vector<int>ans;
+        ans.push_back(cost[0]);
+        for(int i=1;i<cost.size();i++){
+            if(ans[i-1]>cost[i]){
+                ans.push_back(cost[i]);
+            }
+            else{
+                ans.push_back(ans[i-1]);
+            }
+        }
+        return ans;
+    }
+};
