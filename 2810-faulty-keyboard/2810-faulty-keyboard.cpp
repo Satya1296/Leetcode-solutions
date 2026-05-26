f08d1e8a6ddaff19ae15059409a0440666c70aea
Satya1296 <asrithasatya12@gmail.com>
2026-05-26 22:30:49 +0530

    Time: 0 ms (100%), Space: 9.3 MB (64.19%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string res="";
+        for(char ch:s){
+            if(ch!='i'){
+                res+=ch;
+            }
+            else{
+                reverse(res.begin(),res.end());
+            }
+        }
+        return res;
+    }
+};
