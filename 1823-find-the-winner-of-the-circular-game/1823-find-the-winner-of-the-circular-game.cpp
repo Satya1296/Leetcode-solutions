c2cf4db8163ddf0acf5f83375de7a940746b4da6
Patnala Asritha Satya <asrithasatya12@gmail.com>
2026-09-16 06:48:30 +0530

    Time: 1 ms (53.74%), Space: 8.3 MB (50.27%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
+    void solve(vector<int>&res,int j,int &k){
+        if(res.size()==1) return;
+        int d=(j+k-1)%res.size();
+        res.erase(res.begin()+d);
+        solve(res,d,k);
+    }
     int findTheWinner(int n, int k) {
         vector<int>res;
         for(int i=1;i<=n;i++){
             res.push_back(i);
         }
-        // 1 2 3 4 5 
-        int j=0;
-        while(res.size()>1){
-            int d=(j+k-1)%res.size(); //1
-            res.erase(res.begin()+d);
-            j=d;
-        }
+        solve(res,0,k);
         return res[0];
     }
 };
